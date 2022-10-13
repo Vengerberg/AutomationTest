3d346794394c1a34a71bada58b19c764f2098444
Vengerberg <107933528+Vengerberg@users.noreply.github.com>
2022-10-13 12:04:09 +0200

    uBlockOrigin Extension
    Added packed uBlockOrigin extension following ChromeDriver instructions and set ChromeDriver to use it to avoid ads

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,10 +2,14 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
+import com.github.javafaker.Faker;
+
+import java.io.File;
 import java.time.Duration;
 
 public abstract class BaseTest {
@@ -13,19 +17,70 @@ public abstract class BaseTest {
     protected static WebDriver driver;
     protected static Actions actions;
     protected static WebDriverWait wait;
+    protected static Faker faker;
+    protected static String email;
+    protected static String password;
+    protected static String firstName;
+    protected static String lastName;
+    protected static String phone;
+    protected static String birthDay;
+    protected static String birthMonth;
+    protected static String birthYear;
+
+    // address
+    protected static String address;
+    protected static String country;
+    protected static String city;
+    protected static String state;
+    protected static String zip;
+
+    // card details
+    protected static String creditCard;
+    protected static String cvc;
+    protected static String expirationMonth;
+    protected static String expirationYear;
 
     protected static String baseURL = "https://www.automationexercise.com/";
 
+
     // Before/After Setup/Cleanup
 
-    @BeforeSuite
+    @BeforeTest
     public void initializeVariables() {
         System.setProperty("webdriver.chrome.driver",
                 "C:\\Windows\\chromedriver_win32\\chromedriver.exe");
 
-        driver = new ChromeDriver();
+        // https://chromedriver.chromium.org/extensions <-- Installing extensions reference
+        ChromeOptions options = new ChromeOptions();
+        options.addExtensions(new File("extensions/ublock.crx"));
+
+        driver = new ChromeDriver(options);
         actions = new Actions(driver);
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        faker = new Faker();
+
+        email = faker.bothify("???????9#@gmail.com");
+        password = faker.bothify("????????###");
+        firstName = faker.name().lastName();
+        lastName = faker.name().lastName();
+        phone = faker.phoneNumber().cellPhone();
+
+        birthDay = faker.numerify("#");
+        birthMonth = faker.numerify("#");
+        birthYear = faker.numerify("19##");
+
+        address = faker.address().secondaryAddress();
+        country = "United States";
+        city = faker.address().city();
+        state = faker.address().state();
+        zip = faker.address().zipCode();
+
+        creditCard = faker.numerify("################");
+        cvc = faker.numerify("###");
+        expirationMonth = faker.numerify("##");
+        expirationYear = faker.numerify("####");
+
     }
 
     @BeforeMethod
@@ -36,6 +91,7 @@ public abstract class BaseTest {
 
     @AfterSuite
     public void close() {
+        // check for soft assert errors
         driver.close();
         driver.quit();
     }
